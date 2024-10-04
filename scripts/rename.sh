1d54ca2bcd7f352944875ab96f828971f561d4ee
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-10-04 12:37:39 +0300

    fix: add missing eval to properly expand sed command
    - Added eval to ensure the $sed_i_flag is expanded correctly in sed commands.

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -14,14 +14,14 @@ fi
 name=$(printf '%s\n' "$1" | sed 's/[\/&]/\\&/g')
 
 mv include/myprog/myprog.h include/myprog/$name.h
-sed $sed_i_flag "s/myprog/$name/g" include/myprog/$name.h
+eval sed $sed_i_flag "s/myprog/$name/g" include/myprog/$name.h
 
 mv include/myprog include/$name
 
-sed $sed_i_flag "s/myprog/$name/g" scripts/run.sh
-sed $sed_i_flag "s/myprog/$name/g" src/main.c
-sed $sed_i_flag "s/myprog/$name/g" .gitignore
-sed $sed_i_flag "s/myprog/$name/g" build.ninja
+eval sed $sed_i_flag "s/myprog/$name/g" scripts/run.sh
+eval sed $sed_i_flag "s/myprog/$name/g" src/main.c
+eval sed $sed_i_flag "s/myprog/$name/g" .gitignore
+eval sed $sed_i_flag "s/myprog/$name/g" build.ninja
 
 if [ -f "myprog" ]; then
     mv "myprog" $name
